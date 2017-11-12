72a94a9c9c0f40032f267aaf100b0ddc49c89cb7
Shawn Patrick Rice <rice@shawnrice.org>
2017-11-11 19:45:07 -0500

    add in alfred prefs

---
+++ macos/apps/alfred.sh
@@ -0,0 +1,3 @@
+defaults write com.runningwithcrayons.Alfred-3 betacool -bool true
+defaults write com.runningwithcrayons.Alfred-3 "appearance.screen" -bool false
+defaults write com.runningwithcrayons.Alfred-Preferences-3 syncfolder -string "~/Dropbox/app syncing/alfred3"
