078ea88fb6b277df6dd18eca229bd7f1b038173c
Shawn Patrick Rice <shawn@ashbyhq.com>
2026-02-13 11:07:01 -0500

    fix whichkey

--- nvim/zeta.nvim/lua/plugins/which-key.lua
+++ nvim/zeta.nvim/lua/plugins/which-key.lua
@@ -1,15 +1,33 @@
 local M = {
 	"folke/which-key.nvim",
-	lazy = true,
-	keys = {
-		{
-			"<leader>?",
-			function()
-				require("which-key").show({ global = true })
-			end,
-			desc = "Buffer local key maps",
-		},
-	},
+	event = "VeryLazy",
+	config = function()
+		local wk = require("which-key")
+		wk.setup({
+			icons = {
+				separator = "→",
+				group = "+",
+			},
+			disable = { filetypes = { "dashboard", "alpha" } },
+			win = {
+				border = "rounded",
+				padding = { 1, 3 },
+			},
+			triggers = { "<auto>" },
+		})
+
+		wk.add({
+			{ "<leader>f", group = "Find" },
+			{ "<leader>g", group = "Git" },
+			{ "<leader>w", group = "Workspace" },
+			{ "<leader>d", group = "Diagnostics" },
+			{ "<leader>s", group = "Symbols" },
+			{ "<leader>l", group = "LSP" },
+			{ "<leader>c", group = "Code" },
+			{ "<leader>v", group = "View" },
+			{ "<leader><leader>", group = "Session" },
+		})
+	end,
 }
 
 return M
